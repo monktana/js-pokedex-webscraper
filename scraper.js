be6772af2c9e3e4d79acf9693b6fa701d9772f84
monktana <svengiebelmann@gmail.com>
2021-10-20 22:10:42 +0200

    feature: base of type matchup scrap

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import { load } from 'cheerio';
 
+// Pokedex Scrap
 (async () => {
   const response = await fetch('https://pokemondb.net/pokedex/national');
   const html = await response.text();
@@ -8,13 +9,24 @@ import { load } from 'cheerio';
   const $ = load(html);
   const infocards = $('div.infocard-list div.infocard');
 
-  infocards.each((i, el) => {
-    const e = $(el);
+  infocards.each((index, element) => {
+    const el = $(element);
 
     console.log({
-      'name': e.find('a.ent-name').text().toLowerCase(),
+      'name': el.find('a.ent-name').text().toLowerCase(),
       'picture': '',
-      'types': e.find('small a.itype').toArray().map((el) => $(el).text().toLowerCase())
+      'types': el.find('small a.itype').toArray().map((e) => $(e).text().toLowerCase())
     })}
   )
+})()
+
+// Type Scrap
+(async () => {
+  const response = await fetch('https://pokemondb.net/type');
+  const html = await response.text();
+
+  const $ = load(html);
+  const typetable = $('table.type-table');
+  const defensetypes = typetable.find('thead tr th a.type-cell').map((index, element) => $(element).attr('title').toLowerCase()).toArray();
+  const matchups = typetable.find('tbody tr');
 })()

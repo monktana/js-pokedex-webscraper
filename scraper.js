ce256a1713d53a9103071ab29e2e2bdd8926bde7
monktana <svengiebelmann@gmail.com>
2021-10-22 11:18:44 +0200

    feature: type scrap done + saving scraps to files

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import { load } from 'cheerio';
+import fs from 'fs';
 
 // Pokedex Scrap
 (async () => {
@@ -9,16 +10,21 @@ import { load } from 'cheerio';
   const $ = load(html);
   const infocards = $('div.infocard-list div.infocard');
 
+  let pokemon = {};
+  
   infocards.each((index, element) => {
     const el = $(element);
 
-    console.log({
+    pokemon[index] = {
       'name': el.find('a.ent-name').text().toLowerCase(),
       'picture': '',
       'types': el.find('small a.itype').toArray().map((e) => $(e).text().toLowerCase())
-    })}
-  )
-})()
+    }
+  });
+
+  await fs.writeFile('./docs/pokemon.json', JSON.stringify(pokemon), console.log);
+
+})();
 
 // Type Scrap
 (async () => {
@@ -26,7 +32,36 @@ import { load } from 'cheerio';
   const html = await response.text();
 
   const $ = load(html);
-  const typetable = $('table.type-table');
-  const defensetypes = typetable.find('thead tr th a.type-cell').map((index, element) => $(element).attr('title').toLowerCase()).toArray();
-  const matchups = typetable.find('tbody tr');
-})()
+  const typetable = $('table.type-table tbody td');
+
+  let typeMap = {};
+
+  typetable.each((index, element) => {
+    const td = $(element);
+    
+    const [attacker, defender, effectivness] = td.attr('title').split(/[→=]/).map(str => str.trim().toLowerCase());
+
+    if (typeMap[attacker] == undefined) {
+      typeMap[attacker] = {}
+    }
+
+    typeMap[attacker][defender] = parseEffectiveness(effectivness);
+  })
+
+  await fs.writeFile('./docs/typematchups.json', JSON.stringify(typeMap), console.log);
+})();
+
+function parseEffectiveness(effectivness) {
+  switch (effectivness) {
+    case 'super-effective':
+      return 2;
+    case 'normal effectiveness':
+      return 1;
+    case 'not very effective':
+      return 0.5;
+    case 'no effect':
+      return 0;
+    default:
+      throw new Error();
+  }
+}

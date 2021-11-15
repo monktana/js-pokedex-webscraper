baf61a428f074102b5b6d80a4d79a9d96ed1280a
monktana <svengiebelmann@gmail.com>
2021-11-15 21:28:04 +0100

    caching of the single html files, name scrap

--- scraper.js
+++ scraper.js
@@ -2,31 +2,81 @@ import fetch from 'node-fetch';
 import { load } from 'cheerio';
 import fs from 'fs';
 
-// Pokedex Scrap
+// Get all pokemon documents
 (async () => {
-  const response = await fetch('https://pokemondb.net/pokedex/national');
+  const base = 'https://pokemondb.net';
+
+  const response = await fetch(`${base}/pokedex/national`);
   const html = await response.text();
 
   const $ = load(html);
   const infocards = $('div.infocard-list div.infocard');
 
-  let pokemon = {};
-  
-  infocards.each((index, element) => {
-    const el = $(element);
+  const pokemonPages = [];
 
-    pokemon[index] = {
-      'name': el.find('a.ent-name').text().toLowerCase(),
-      'picture': '',
-      'types': el.find('small a.itype').toArray().map((e) => $(e).text().toLowerCase())
-    }
-  });
+  for (const [index, infocard] of infocards.toArray().entries()) {
+    const el = $(infocard);
+    const pokemonURI = el.find('div.infocard span.infocard-lg-img a').attr('href');
+    const pokemonPage = await fetch(`${base}${pokemonURI}`);
 
-  await fs.writeFile('./docs/pokemon.json', JSON.stringify(pokemon), console.log);
+    pokemonPages.push(pokemonPage);
+  }
 
+  for (const [index, page] of pokemonPages.entries()) {
+    const test = await page.text();
+    await fs.writeFile(`./docs/pages/pokemon_${index + 1}.html`, test, console.log);
+  }
 })();
 
-// Type Scrap
+// Pokedex
+(async () => {
+  const pokemonFiles = fs.readdirSync('./docs/pages');
+  const pokemon = {};
+
+  for (const [index, file] of pokemonFiles.entries()) {
+    const html = fs.readFileSync(`./docs/pages/${file}`, console.log);
+    const $ = load(html);
+
+    const infoTable = $('div.sv-tabs-panel.active div.grid-col.span-md-6.span-lg-4 table.vitals-table tbody');
+    const dexNumber = parseInt(infoTable.find('tr:nth-child(1) td strong').text());
+
+    pokemon[dexNumber] = {
+      'name': $('h1').text().toLowerCase(),
+      'picture': $('a[rel=lightbox]').attr('href'),
+      'types': infoTable.find('tr:nth-child(2) td a').toArray().map((e) => $(e).text().toLowerCase())
+    };
+  }
+
+  await fs.writeFile('./docs/pokemon.json', JSON.stringify(pokemon), console.log);
+})();
+
+// Names
+(async () => {
+  const pokemonFiles = fs.readdirSync('./docs/pages');
+  const languages = {
+    de: {},
+    en: {},
+  };
+
+  for (const [index, file] of pokemonFiles.entries()) {
+    const html = fs.readFileSync(`./docs/pages/${file}`, console.log);
+    const $ = load(html);
+
+    const nameTable = $('#main > div:nth-child(28) > div:nth-child(1) > div > table.vitals-table > tbody');
+    const nameEN = nameTable.find('tr:nth-child(1) td').text().toLowerCase();
+    const nameDE = nameTable.find('tr:nth-child(3) td').text().toLowerCase();
+
+    languages['de'][nameDE] = {
+      'en': nameEN,
+    };
+    languages['en'][nameEN] = {
+      'de': nameDE,
+    };
+  }
+  await fs.writeFile('./docs/names.json', JSON.stringify(languages), console.log);
+})();
+
+// Type matchups
 (async () => {
   const response = await fetch('https://pokemondb.net/type');
   const html = await response.text();
@@ -48,7 +98,7 @@ import fs from 'fs';
     typeMap[attacker][defender] = parseEffectiveness(effectivness);
   })
 
-  await fs.writeFile('./docs/typematchups.json', JSON.stringify(typeMap), console.log);
+  await fs.writeFile('./docs/type-matchups.json', JSON.stringify(typeMap), console.log);
 })();
 
 function parseEffectiveness(effectivness) {
